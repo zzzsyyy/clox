bb82f18f5262407575f9b5a5c0a10a325df34f78
zzzsyyy <me@zzzsy.top>
2023-10-06 20:26:02 +0800

    feat: add `for` support

--- src/compiler.c
+++ src/compiler.c
@@ -509,6 +509,47 @@ static void expression_statement() {
 	emit_byte(OP_POP);
 }
 
+static void for_statement() {
+	begin_scope();
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+	if (match(TOKEN_SEMICOLON)) {
+		// No initializer
+	} else if (match(TOKEN_VAR)) {
+		var_declaration();
+	} else {
+		expression_statement();
+	}
+
+	int loop_start = current_chunk()->count;
+	int exit_jump = -1;
+	if (!match(TOKEN_SEMICOLON)) {
+		expression();
+		consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+		exit_jump = emit_jump(OP_JUMP_IF_FALSE);
+		emit_byte(OP_POP);
+	}
+
+	if (!match(TOKEN_RIGHT_PAREN)) {
+		int body_jump = emit_jump(OP_JUMP);
+		int increment_start = current_chunk()->count;
+		expression();
+		emit_byte(OP_POP);
+		consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+		emit_loop(loop_start);
+		loop_start = increment_start;
+		patch_jump(body_jump);
+	}
+
+	statement();
+	emit_loop(loop_start);
+	if (exit_jump != -1) {
+		patch_jump(exit_jump);
+		emit_byte(OP_POP);
+	}
+	end_scope();
+}
+
 static void if_statement() {
 	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
 	expression();
@@ -526,6 +567,8 @@ static void if_statement() {
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		print_statement();
+	} else if (match(TOKEN_FOR)) {
+		for_statement();
 	} else if (match(TOKEN_IF)){
 		if_statement();
 	} else if (match(TOKEN_WHILE)) {
